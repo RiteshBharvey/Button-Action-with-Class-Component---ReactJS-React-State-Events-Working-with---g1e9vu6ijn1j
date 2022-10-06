eec0d609084a44ee2d6aff68b0d28b61bd7e11ce
RiteshBharvey <99973928+RiteshBharvey@users.noreply.github.com>
2022-10-07 01:39:45 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -17,16 +17,16 @@ import '../styles/App.css';
 class App extends Component{
 	constructor(props){
 		super(props);
-		this.state={para:""}
+		this.state={para:"",hide:"false"}
 	}
 	clickHandler(){
-		this.setState({para:"Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy"})
+		this.setState({para:"Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy",hide:"true"})
 	}
 	render(){
 		return(
     		<div id="main">
-			<p id="para">{this.state.para}</p>
-			<button type="button" id="click" onClick={this.clickHandler}>Click</button>
+			<button type="button" id="click" onClick={this.clickHandler.bind(this)}>Click</button>
+			{this.state.hide && <p id="para">{this.state.para}</p>}
     		</div>
     		);
 	}
