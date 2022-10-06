b60fc261fcdf10fa8f81a9b73a701f5fb009a673
RiteshBharvey <99973928+RiteshBharvey@users.noreply.github.com>
2022-10-07 02:55:10 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,6 @@
 import React, {Component} from "react";
 import '../styles/App.css';
 
-// const App = ()=>{
-// 	const[para,setPara]=useState("");
-// 	const clickHandler = ()=>{
-// 		setPara("Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy");
-// 	}
-//     	return(
-//     		<div id="main">
-// 			<p id="para">{para}</p>
-// 			<button id="click" onClick={clickHandler}>Click</button>
-//     		</div>
-//     	);
-// }
-
 class App extends Component{
 	constructor(){
 		super();
