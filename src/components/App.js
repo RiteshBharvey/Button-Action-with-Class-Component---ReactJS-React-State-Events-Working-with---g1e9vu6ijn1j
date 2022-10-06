f8b7a6e47003b86cf59901906bd1c895ef3a1ff3
RiteshBharvey <99973928+RiteshBharvey@users.noreply.github.com>
2022-10-07 00:39:59 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,35 @@
-import React, {Component, useState} from "react";
+import React, {Component} from "react";
 import '../styles/App.css';
 
-const App = ()=>{
-	const[para,setPara]=useState("");
-	const clickHandler = ()=>{
-		setPara("Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy");
+// const App = ()=>{
+// 	const[para,setPara]=useState("");
+// 	const clickHandler = ()=>{
+// 		setPara("Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy");
+// 	}
+//     	return(
+//     		<div id="main">
+// 			<p id="para">{para}</p>
+// 			<button id="click" onClick={clickHandler}>Click</button>
+//     		</div>
+//     	);
+// }
+
+class App extends Component{
+	constructor(){
+		super(props)
+		this.state={para:""}
 	}
-    	return(
+	clickHandler=()=>{
+		this.setState({para:"Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy"})
+	}
+	render(){
+		return(
     		<div id="main">
-			<p id="para">{para}</p>
-			<button id="click" onClick={clickHandler}>Click</button>
+			<p id="para">{this.state.para}</p>
+			<button id="click" onClick={this.clickHandler}>Click</button>
     		</div>
-    	);
+    		);
+	}
 }
 
 
